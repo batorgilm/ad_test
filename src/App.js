306d7f0c21a475a527b7bdf4b80ccbb45485ee7b
batorgilm <batorgil.m@nest.edu.mn>
2023-01-12 17:09:10 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <div
+        style={{
+          position: "absolute",
+          left: "0",
+          top: "0",
+          backgroundColor: "white",
+        }}
+        dangerouslySetInnerHTML={{
+          __html:
+            "<iframe id='a1d272e9' name='a1d272e9' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='https://175.41.177.147/revive/www/delivery/ck.php?n=ada81b5a&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ada81b5a' border='0' alt='' /></a></iframe>",
+        }}
+      ></div>
     </div>
   );
 }
