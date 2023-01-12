0a38ab36763b830f3de72723ccb8f8e555506748
batorgilm <batorgil.m@nest.edu.mn>
2023-01-12 17:21:37 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const renderAd = () => {
+    return {
+      __html:
+        "<iframe id='a1d272e9' name='a1d272e9' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='https://175.41.177.147/revive/www/delivery/ck.php?n=ada81b5a&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ada81b5a' border='0' alt='' /></a></iframe>",
+    };
+  };
   return (
     <div className="App">
+      <div dangerouslySetInnerHTML={renderAd()}></div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,18 +27,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <div
-        style={{
-          position: "absolute",
-          left: "0",
-          top: "0",
-          backgroundColor: "white",
-        }}
-        dangerouslySetInnerHTML={{
-          __html:
-            "<iframe id='a1d272e9' name='a1d272e9' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='https://175.41.177.147/revive/www/delivery/ck.php?n=ada81b5a&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ada81b5a' border='0' alt='' /></a></iframe>",
-        }}
-      ></div>
     </div>
   );
 }
