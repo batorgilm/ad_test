db02464f057be5d0f42547edec666e9de6f368e7
batorgilm <batorgil.m@nest.edu.mn>
2023-01-12 17:43:36 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useRef } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("pinecone effect");
-  }, []);
-  const renderAd = () => {
-    return {
-      __html:
-        "<iframe id='a1d272e9' name='a1d272e9' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=123' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='http://175.41.177.147/revive/www/delivery/ck.php?n=ada81b5a&amp;cb=123' target='_blank'><img src='http://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=123&amp;n=ada81b5a' border='0' alt='' /></a></iframe>",
-    };
-  };
   return (
     <div className="App">
-      <div dangerouslySetInnerHTML={renderAd()}></div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html:
+            "<iframe id='a81b9a17' name='a81b9a17' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='http://175.41.177.147/revive/www/delivery/ck.php?n=a96f42e6&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a96f42e6' border='0' alt='' /></a></iframe>",
+        }}
+      ></div>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
