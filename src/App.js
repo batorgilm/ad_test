ab9743ba346ee7ecfe1696b8a1ed55a703f46cca
batorgilm <batorgil.m@nest.edu.mn>
2023-01-16 17:06:31 +0800

    allow insecure

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <div
         dangerouslySetInnerHTML={{
-          __html:
-            "<iframe id='a81b9a17' name='a81b9a17' src='http://175.41.177.147/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='988' height='256' allow='autoplay'><a href='http://175.41.177.147/revive/www/delivery/ck.php?n=a96f42e6&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://175.41.177.147/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a96f42e6' border='0' alt='' /></a></iframe>",
+          __html: `<iframe id='a9960d98' name='a9960d98' src='http://18.141.180.51/revive/www/delivery/afr.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='468' height='60' allow='autoplay'><a href='http://18.141.180.51/revive/www/delivery/ck.php?n=a7fc8764&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://18.141.180.51/revive/www/delivery/avw.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a7fc8764' border='0' alt='' /></a></iframe>
+          `,
         }}
       ></div>
 
