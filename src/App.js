f6a8d299542b99b7daa7a157279957aa9d8a0161
batorgilm <batorgil.m@nest.edu.mn>
2023-01-18 15:37:41 +0800

    change banner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/iframe-has-title */
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<iframe id='a9960d98' name='a9960d98' src='http://18.141.180.51/revive/www/delivery/afr.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='468' height='60' allow='autoplay'><a href='http://18.141.180.51/revive/www/delivery/ck.php?n=a7fc8764&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://18.141.180.51/revive/www/delivery/avw.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a7fc8764' border='0' alt='' /></a></iframe>
-          `,
-        }}
-      ></div>
-
+      <iframe
+        id="a0530e1e"
+        name="a0530e1e"
+        src="http://18.141.180.51/revive/www/delivery/afr.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+        frameborder="0"
+        scrolling="no"
+        width="1435"
+        height="406"
+        allow="autoplay"
+      >
+        <a
+          href="http://18.141.180.51/revive/www/delivery/ck.php?n=a05f905a&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+          target="_blank"
+        >
+          <img
+            src="http://18.141.180.51/revive/www/delivery/avw.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a05f905a"
+            border="0"
+            alt=""
+          />
+        </a>
+      </iframe>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
