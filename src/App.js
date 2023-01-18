83dea29047022defe63a8d14f2b0d12d339bc82d
batorgilm <batorgil.m@nest.edu.mn>
2023-01-18 16:43:51 +0800

    ssh

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <iframe
-        id="a0530e1e"
-        name="a0530e1e"
-        src="http://18.141.180.51/revive/www/delivery/afr.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+        id="ab7e3fd6"
+        name="ab7e3fd6"
+        src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE"
         frameborder="0"
         scrolling="no"
         width="1435"
@@ -17,16 +17,39 @@ function App() {
         allow="autoplay"
       >
         <a
-          href="http://18.141.180.51/revive/www/delivery/ck.php?n=a05f905a&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+          href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a6278418&amp;cb=INSERT_RANDOM_NUMBER_HERE"
           target="_blank"
         >
           <img
-            src="http://18.141.180.51/revive/www/delivery/avw.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a05f905a"
+            src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a6278418"
             border="0"
             alt=""
           />
         </a>
       </iframe>
+
+      <iframe
+        id="a97778f0"
+        name="a97778f0"
+        src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=22&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+        frameborder="0"
+        scrolling="no"
+        width="1440"
+        height="517"
+        allow="autoplay"
+      >
+        <a
+          href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a1cef479&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+          target="_blank"
+        >
+          <img
+            src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=22&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a1cef479"
+            border="0"
+            alt=""
+          />
+        </a>
+      </iframe>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -39,9 +62,9 @@ function App() {
           rel="noopener noreferrer"
         >
           <iframe
-            id="ad449bc5"
-            name="ad449bc5"
-            src="http://18.141.180.51/revive/www/delivery/afr.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+            id="a9dcf379"
+            name="a9dcf379"
+            src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE"
             frameborder="0"
             scrolling="no"
             width="914"
@@ -49,11 +72,11 @@ function App() {
             allow="autoplay"
           >
             <a
-              href="http://18.141.180.51/revive/www/delivery/ck.php?n=ae57a35a&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+              href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a91ab58d&amp;cb=INSERT_RANDOM_NUMBER_HERE"
               target="_blank"
             >
               <img
-                src="http://18.141.180.51/revive/www/delivery/avw.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ae57a35a"
+                src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a91ab58d"
                 border="0"
                 alt=""
               />
