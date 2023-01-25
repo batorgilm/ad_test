06c939f88c0ccb421c86312459a7100e551ce1bf
batorgilm <batorgil.m@nest.edu.mn>
2023-01-25 15:52:06 +0800

    a

--- src/App.js
+++ src/App.js
@@ -6,50 +6,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <iframe
-        id="ab7e3fd6"
-        name="ab7e3fd6"
-        src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-        frameborder="0"
-        scrolling="no"
-        width="1435"
-        height="406"
-        allow="autoplay"
-      >
-        <a
-          href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a6278418&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-          target="_blank"
-        >
-          <img
-            src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=20&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a6278418"
-            border="0"
-            alt=""
-          />
-        </a>
-      </iframe>
-
-      <iframe
-        id="a97778f0"
-        name="a97778f0"
-        src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=22&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-        frameborder="0"
-        scrolling="no"
-        width="1440"
-        height="517"
-        allow="autoplay"
-      >
-        <a
-          href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a1cef479&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-          target="_blank"
-        >
-          <img
-            src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=22&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a1cef479"
-            border="0"
-            alt=""
-          />
-        </a>
-      </iframe>
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -61,27 +17,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <iframe
-            id="a9dcf379"
-            name="a9dcf379"
-            src="https://revive.test-lms-service.click/revive/www/delivery/afr.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-            frameborder="0"
-            scrolling="no"
-            width="914"
-            height="1046"
-            allow="autoplay"
-          >
-            <a
-              href="https://revive.test-lms-service.click/revive/www/delivery/ck.php?n=a91ab58d&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-              target="_blank"
-            >
-              <img
-                src="https://revive.test-lms-service.click/revive/www/delivery/avw.php?zoneid=21&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a91ab58d"
-                border="0"
-                alt=""
-              />
-            </a>
-          </iframe>
           Learn React
         </a>
       </header>
